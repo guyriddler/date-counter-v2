2ec582bd2d5e262257a995be4409aed8b5041519
guyriddler <guyriddler@gmail.com>
2024-05-05 19:59:02 -0500

    Added step slider control and text input field for count.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+  console.log(date);
+
+  return (
+    <div>
+      <h1>Date Counter Application!</h1>
+
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+
+        <p className="countmessage">Step: {step}</p>
+      </div>
+
+      <div>
+        <button
+          onClick={() => setCount((c) => c - step)}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          -
+        </button>
+
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+
+        <button
+          onClick={(e) => setCount((c) => c + step)}
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+        >
+          +
+        </button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}
